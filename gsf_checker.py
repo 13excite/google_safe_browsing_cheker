78ae17a8df7e9ab5fabf6136cceaff8f8afd9afe
Vladimir Dulenov <v.dulenov@v.dulenov>
2018-03-11 22:20:37 +0300

    update checker

--- gsf_checker.py
+++ gsf_checker.py
@@ -1,10 +1,22 @@
 import requests
 import json
+import argparse
 import sys
 
 URL = 'https://safebrowsing.googleapis.com/v4/threatMatches:find?key='
-KEY = 'trololo'
 FILENAME = '/tmp/check.txt'
+
+def get_url(file):
+    """
+    return safebrowsing url with key;
+    accept path to key file
+    """
+    try:
+        with open(file, 'r') as f:
+            return URL + f.readlines()[0].rstrip()
+    except IOError as err:
+        print("Failed with error: %s" % err)
+        sys.exit(1)
 
 def get_json(url, data):
     json_data = json.dumps(data)
@@ -12,12 +24,15 @@ def get_json(url, data):
         'Content-Type': 'application/json'
     }
     try:
-        r = requests.post(url, json_data, headers=headers)
-    except requests.exceptions.RequestException as e:
+        r = requests.post(url, json_data, headers=headers, timeout=5)
+    except requests.exceptions.RequestException as err:
+        print("Request error: %s" % err)
         sys.exit(1)
-    return json.loads(r.text.encode('UTF-8'))
+    except requests.exceptions.Timeout as err:
+        print("Request timeout 5 seconds with error: %s" % err)
+        sys.exit(1)
+    return r.json()
 
-valid_url =  URL+KEY
 data = {
         "client": {
                     "clientId":      "myproject",
@@ -35,15 +50,18 @@ data = {
 }
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Google Safe browser checker')
+    parser.add_argument('--key', '-k', required=True, help='specify the path to  key file')
+    path_to_key = parser.parse_args().key
 
-    output = get_json(valid_url, data)
+    output = get_json(get_url(path_to_key), data)
+    #print(output)
 
     if len(output) > 0:
-        list_data =  output.get('matches'.encode('UTF-8')) #.get('threatType'.encode('UTF-8'))
+        list_data =  output.get('matches')
         with open(FILENAME, 'a') as f:
             for elem in list_data:
-                f.write(elem.get('threat'.encode('UTF-8')).get('url'.encode('UTF-8')) + '\n')
-        f.close()
+                f.writelines(elem.get('threat').get('url') + '\n')
     else:
         open(FILENAME, 'w').close()
         #print "OK"
